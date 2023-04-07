2b8e316933270b67f9f5c6bcfa1008c79473ba93
Naveenkumar S E <naveen42moon@gmail.com>
2023-04-07 16:25:39 +0530

    responsive-navbar-updated

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -14,7 +14,7 @@ const NavBar = () => {
             {/* <img className=" ml-5" alt="kec logo" style={{width:"calc(100%*0.09)"}} src = {keclogo} ></img> */}
             <div className="text-dark flex justify-between items-center h-24 w-4/5 max-w-[1240px] ">
 
-                <Link to="/"><h1 className="w-full text-3xl font-bold px-9 font-poppins"> Sri Amman Home Appliances </h1></Link>
+                <Link to="/"><h1 className="w-full text-2xl md:text-4xl  font-bold px-9 font-poppins "> Sri Amman Home Appliances </h1></Link>
 
                 <ul className="hidden md:flex ">
                     <li className="p-4"><Link to="/">Home</Link></li>
@@ -22,12 +22,12 @@ const NavBar = () => {
                     <li className="p-4"><Link to="/">About</Link></li>
                     <li className="p-4"><Link to="/">Login/Register</Link></li>
                 </ul>
-                <div onClick={handleNav} className='block md:hidden'>
+                <div onClick={handleNav} className='block md:hidden '>
                     {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
 
                 </div>
                 <div className={nav ? 'z-10 fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500 text-white' : 'fixed left-[-100%]'}>
-                    <h1 className="w-full text-3xl font-bold  m-4"> Sri Amman Home Appliances </h1>
+                    <h1 className="w-full text-2xl font-bold  m-2 font-poppins "> Sri Amman Home Appliances </h1>
                     <ul className="uppercase p-4">
                         <li className="p-4 border-b border-gray-600"><Link to="/">Home</Link></li>
                         <li className="p-4 border-b border-gray-600"><Link to="/">Event</Link></li>
