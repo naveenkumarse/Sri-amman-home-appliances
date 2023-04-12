258f8507d827ef53c53f3f7fc525eaf4d54117f3
Naveenkumar S E <naveen42moon@gmail.com>
2023-04-13 00:07:04 +0530

    Update register.js

--- client/src/components/login/register.js
+++ client/src/components/login/register.js
@@ -219,7 +219,7 @@ const Register = () => {
                 </div>
                 <br />
                 <div class="w-full justify-center px-3 mb-6 md:mb-0 ">
-                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" onClick={onSignup()} >
+                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" onClick={onSignup} >
                         Register
                     </button>
 
