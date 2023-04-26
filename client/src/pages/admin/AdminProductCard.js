1312a020e8a74734cf29e7edc5673329459eba27
Naveenkumar S E <naveen42moon@gmail.com>
2023-04-26 06:43:40 +0530

    admin-product-update-added

--- client/src/pages/admin/AdminProductCard.js
+++ client/src/pages/admin/AdminProductCard.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Button, Label, Modal, TextInput, Textarea } from "flowbite-react";
+// import { Button, Label, Modal, TextInput, Textarea } from "flowbite-react";
 import { deleteProduct, updateProduct } from "../../api";
 import {  useNavigate } from "react-router-dom";
 
@@ -7,9 +7,10 @@ const AdminProductCard = ({ product }) => {
   
   const navigate = useNavigate();
   const { image, name, description, price,stocks } = product;
-  const [uname,setUname] = useState();
-  const [uprice,setUprice] = useState();
-  const [udesc,setUdesc] = useState();
+  const [uname,setUname] = useState(name);
+  const [uprice,setUprice] = useState(price);
+  const [udesc,setUdesc] = useState(description);
+  const [stock,setStock] = useState(stocks);
    
   const  Uname = product.name  ;
   const  Udescription  =  product.description ;
@@ -24,11 +25,12 @@ const AdminProductCard = ({ product }) => {
 
 
   const updateOneProduct =()=>{
-    const body = {pid,uname,uprice,udesc,image,stocks}
+    const body = {pid,uname,uprice,udesc,image,stock}
+    console.log(body)
     updateProduct(body);
     window.location.reload();
   }
-  const [modelShow, updateModelShow] = useState(false);
+  // const [modelShow, updateModelShow] = useState(false);
 
   return (
     <div className="rounded-2xl overflow-hidden  shadow-xl tranform hover:scale-110 duration-100">
@@ -41,26 +43,26 @@ const AdminProductCard = ({ product }) => {
       </div>
       <div className="flex flex-col items-center my-2 py-2">
         <span className="font-body text-slate-500 block font-bold text-xl">
-          {name}
+         {/* <input defaultValue={name} value={uname} onChange={(e)=>setUname(e.target.value)}/> */}
+         {name}
         </span>
         <center>
-          <span className="font-body text-slate-500 block my-3">
-            {description}
+          <span className="font-body text-slate-500 block my-3 ">
+            {Udescription}
+              {/* <input defaultValue={name} value={uname} onChange={(e)=>setUname(e.target.value)}/> */}
           </span>
         </center>
         <center>
           <span className="font-body text-slate-500 block my-3">
-            Stocks: <span className="text-black"> <input value={stocks} /></span>
+            Stocks: <span className="text-black"> <input type="number" value={stock} defaultValue={stocks} onChange={(e)=>setStock(e.target.value)} /></span>
           </span>
 
         </center>
-        <span className="font-body text-slate-500">₹<input value={uprice} defaultValue={price} onChange={(e)=>setUprice(e.target.value)}/></span>
+        <span className="font-body text-slate-500">Price: ₹<input type="number" value={uprice} defaultValue={price} onChange={(e)=>setUprice(e.target.value)}/></span>
         <div className="flex justify-between w-1/2">
           <button
             className="bg-black hover:bg-lblue hover:text-black text-bblue font-bold py-2 px-4 my-3 rounded-full"
-            onClick={() => {
-              updateModelShow(!modelShow);
-            }}
+            onClick={updateOneProduct}
           >
             Update{" "}
           </button>
